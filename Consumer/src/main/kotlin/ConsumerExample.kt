5779edeebcb8f1210c4632ec82c99c14a5c6a86a
Olivia Zoe <zoe.i2k1@gmail.com>
2019-05-01 15:46:53 +0200

    Prepare for merge. Closes #3

--- Consumer/src/main/kotlin/ConsumerExample.kt
+++ Consumer/src/main/kotlin/ConsumerExample.kt
@@ -5,16 +5,47 @@ data class ExampleClass(
     val x: String, val answer: Int, var mutable: Float,
     var the: String = "center"
 ) {
+    private val secret = "Don't tell anyone the password is 1234"
     fun answer() {
         require(answer == 42) { "THE ANSWER IS A LIE!" }
         println("The answer is $answer");
     }
+
+    companion object{
+        @JvmStatic
+        val leaveMeAlone = "Static String"
+        val nonStat = ""
+    }
 }
+@AutoUnpack
+data class Delegated(val title: String) {
+    val x by lazy { 42 }
+}
+
+@AutoUnpack
+data class PrivateData(val one: String, val two: String, private val secret: String)
 
 fun main(a: Array<String>) {
     val cls = ExampleClass("cannot hold!", 42, 3.14159f)
 
+    var size = 0
     for (variable in cls) {
+        size++;
         println(variable)
     }
+    require(size == 4) { "Too many items for public-only" }
+    size = 0;
+    val delegated = Delegated("Hello World!")
+    for (variable in delegated) {
+        size++;
+        println("Variable");
+    }
+    require(size == 2) { "Too many items for the delegate" }
+    size = 0;
+    val privateInclusive = PrivateData("hello", "world", "Don't look at me!")
+    for (variable in privateInclusive) {
+        size++;
+        println(variable)
+    }
+    require(size == 2) { "Too many items for the privacy class" }
 }
--- UnpackProcessor/src/main/kotlin/oliviazoe0/processor/UnpackCodeGenerator.kt
+++ UnpackProcessor/src/main/kotlin/oliviazoe0/processor/UnpackCodeGenerator.kt
@@ -12,7 +12,6 @@ import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic
 
 class UnpackCodeGenerator : AbstractProcessor() {
-
     override fun getSupportedAnnotationTypes(): MutableSet<String> = mutableSetOf(AutoUnpack::class.java.name)
     override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latest()
 
@@ -20,7 +19,6 @@ class UnpackCodeGenerator : AbstractProcessor() {
         // Find elements with the annotation
         val annotatedElements = roundEnv.getElementsAnnotatedWith(AutoUnpack::class.java)
         if(annotatedElements.isEmpty()) {
-            // Self-explanatory
             return false;
         }
         // Iterate the elements
@@ -30,9 +28,9 @@ class UnpackCodeGenerator : AbstractProcessor() {
             val pkg = processingEnv.elementUtils.getPackageOf(element).toString()
             // Then generate the class
             generateClass(name,
-                if (pkg == "unnamed package") "" else pkg, // This is a patch for an issue where classes in the root
-                                                           // package return package as "unnamed package" rather than empty,
-                                                           // which breaks syntax because "package unnamed package" isn't legal.
+                if (pkg == "unnamed package") "" else pkg,  // This is a patch for an issue where classes in the root
+                                                            // package return package as "unnamed package" rather than empty,
+                                                            // which breaks syntax because "package unnamed package" isn't legal.
                 element)
         }
         // Return true for success
@@ -40,14 +38,40 @@ class UnpackCodeGenerator : AbstractProcessor() {
     }
 
     private fun generateClass(className: String, pkg: String, element: Element){
-        val classVariables = element.enclosedElements
-            .filter { it.kind == ElementKind.FIELD } // Grab the variables
+        val elements = element.enclosedElements
+        val classVariables = elements
+            .filter {
+                val name = if (it.simpleName.contains("\$delegate"))
+                    it.simpleName.toString().substring(0, it.simpleName.indexOf("$"))
+                else it.simpleName.toString()
+
+                it.kind == ElementKind.FIELD // Find fields
+                        && Modifier.STATIC !in it.modifiers // that aren't static
+                        // Additionally, we have to ignore private fields. Extension functions can't access these, and accessing
+                        // them is a bad idea anyway. Kotlin lets you expose get without exposing set. If you, by default, don't
+                        // allow access to the getter, there's a high chance exposing it is a bad idea.
+                        && elements.any { getter -> getter.kind == ElementKind.METHOD // find methods
+                                && getter.simpleName.toString() ==
+                                        "get${name[0].toUpperCase().toString() + (if (name.length > 1) name.substring(1) else "")}"
+                                && Modifier.PUBLIC in getter.modifiers
+                        }
+
+
+            } // Grab the variables
+            .map {
+                // Map the name now. Also supports later filtering
+                if (it.simpleName.endsWith("\$delegate")) {
+                    // Support by lazy
+                    it.simpleName.subSequence(0, it.simpleName.indexOf("$"))
+                } else it.simpleName
+            }
+        processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, classVariables.toString())
         if (classVariables.isEmpty()) return; // Self-explanatory
 
         val file = FileSpec.builder(pkg, className)
             .addFunction(FunSpec.builder("iterator") // For automatic unpacking in a for loop
                 .receiver(element.asType().asTypeName().copy()) // Add it as an extension function of the class
-                .addStatement("return listOf(${classVariables.joinToString(", ") { it.simpleName }}).iterator()") // add the return statement. Create a list, push an iterator.
+                .addStatement("return listOf(${classVariables.joinToString(", ")}).iterator()") // add the return statement. Create a list, push an iterator.
                 .addModifiers(KModifier.PUBLIC, KModifier.OPERATOR) // This needs to be public. Because it's an iterator, the function also needs the `operator` keyword
                 .build()
             ).build()
@@ -55,7 +79,7 @@ class UnpackCodeGenerator : AbstractProcessor() {
         // Grab the generate directory.
         val genDir = processingEnv.options["kapt.kotlin.generated"]!!
         // Then write the file.
-        file.writeTo(File(genDir, "${className}Generated.kt"))
+        file.writeTo(File(genDir, "$pkg/${element.simpleName.replace("\\.kt".toRegex(), "")}Generated.kt"))
     }
 
 }
